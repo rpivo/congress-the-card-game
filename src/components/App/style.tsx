39a3115908481390b08503f4858cb7df6c68373c
Ryan Pivovar <ryanpivovar@gmail.com>
2020-08-04 15:24:29 -0400

    update top-level App styles

--- src/components/App/style.tsx
+++ src/components/App/style.tsx
@@ -2,7 +2,10 @@ import styled from 'styled-components';
 
 const Style = styled.div`
   border: 1px solid red;
+  color: #333;
   font-family: Arial;
+  font-size: 14px;
+  font-weight: bold;
 `;
 
 export default Style;
